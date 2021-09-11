bc448bd04e7bcc387a5f9f001d0bec361b9958ec
Lee-alt761 <chongzhili087@gmail.com>
2021-09-11 23:56:24 +0900

    bubble_sort 完了

--- html/knock/bubble_sort/index.php
+++ html/knock/bubble_sort/index.php
@@ -1,9 +1,17 @@
 <?php
 $nums = [100, 5, 2, -10, 8, 10, -1, 15, 1, -100];
 
-$idxLast = count($nums) - 1;
+$idxLast = count($nums) - 1; /* count()は要素の数、つまり配列の数なのでcount($nums)=10 */
 
-//ここにコードを書きましょう。
+for ($i = 0; $i <= $idxLast; $i++) {
+    for ($j = $idxLast; $i <= $j; $j--) {
+        if ($nums[$j] < $nums[$j - 1]) {
+            $k = $nums[$j - 1];
+            $nums[$j - 1] = $nums[$j];
+            $nums[$j] = $k;
+        }
+    }
+}
 
 echo '<pre>';
 print_r($nums);
